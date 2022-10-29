1a3ad0d2740bd49095cd5364c95a658856038ce7
DanilGastarbayter <h2oo2h22@gmail.com>
2022-10-29 13:14:50 +0300

    h

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+class Human:
+    def __init__(self, name = "maynkrafter3000", job = None, home = None, car = None):
+        self.name = name
+        self.job = job
+        self.home = home
+        self.car = car
+        self.gladness = 50
+        self.satiety = 50
+        
